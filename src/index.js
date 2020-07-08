f117e666914e01e5668564670fd96e186dbf87f4
nobuti <buti@nobuti.com>
2020-07-08 04:34:56 +0200

    Eslint integration
    Installed dependencies
    Updated scripts for testing and linting
    Fixed lint errors and warnings

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import socketIOClient from "socket.io-client";
-const ENDPOINT = 'http://localhost:3000';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import socketIOClient from 'socket.io-client'
+import './index.css'
 
-import List from './components/List';
+import List from './components/List'
+const ENDPOINT = 'http://localhost:3000'
 
 const Root = () => {
-    return (
-        <>
-            <h1>League Champion</h1>
-            <List />
-        </>
-    )
+  return (
+    <>
+      <h1>League Champion</h1>
+      <List />
+    </>
+  )
 }
 
-const container = document.getElementById('app');
-ReactDOM.render( <Root />, container );
+const container = document.getElementById('app')
+ReactDOM.render(<Root />, container)
 
-const socket = socketIOClient(ENDPOINT);
-socket.on("update/players", data => {
-    console.log(data);
+const socket = socketIOClient(ENDPOINT)
+socket.on('update/players', data => {
+  console.log('/////////////////////', data)
 })
